831c6a83dfdd4f351bb7a90defeea9b7c4405a5b
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-07 15:47:16 -0700

    UPDATE 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-int main()
+
+/**
+ * main -entry point
+ *
+ * Descriptin : prints "Programming is like a multilingual puzzle"
+ *
+ * return always (0) success
+ * 
+ * 
+ */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
