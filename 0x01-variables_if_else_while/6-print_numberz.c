fa0fd8a5178adc7f39273f6114e97bc6c84578f3
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-08 16:01:57 -0700

    ADD 6-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9 of base 10 number
+ *
+ * Description: print single digits of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	
+	putchar(10);
+
+	return (0);
+}
