a0e6c31b40b5ca3452e3666d1164f817a9f20fcb
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-08 15:45:25 -0700

    ADD 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print letters of the alphabet except e and q
+ *
+ * Description: print letters of the alphabet except e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
