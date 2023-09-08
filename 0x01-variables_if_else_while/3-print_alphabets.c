094239dc2c44cd749e016dbe47063af13a77b4a0
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-08 15:35:35 -0700

    ADD 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters in capital letters
+ *
+ * Description: print the letters in capital letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 97;
+	j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
