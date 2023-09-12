4b1be3f6296f8db2da2f6b13b2eb84129bd3de92
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-12 13:59:02 -0700

    ADD 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print out the alphabet in small letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_alphabet(void)
+{
+	char i = 'a'
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+
+		i++
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
