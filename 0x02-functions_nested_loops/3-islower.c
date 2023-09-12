5759e62e64d33c38546d3fa7f05fae09db7f30b0
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-12 14:23:53 -0700

    ADD 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - detects if a letter is lowercase
+ * 
+ * @c: character to check
+ *
+ * Return: 1 if @c is in lowercase else return o
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
