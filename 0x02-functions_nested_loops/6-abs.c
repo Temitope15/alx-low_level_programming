26df7c956b9099494a70082092f0160f5167d151
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-12 15:11:49 -0700

    ADD 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int_abs - prints the absolutr value of n
+ *
+ * @n: the number to convert
+ *
+ * Return: the absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+
+	return (-n);
+}
