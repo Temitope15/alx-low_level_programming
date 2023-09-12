29625cd624fcb011d2374aa580290cbca30a8d74
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-11 20:03:49 -0700

    ADD 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - print number combination separated by comma and full stop
+ *
+ * Description: print two digit numbers in ascending order
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 57 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+
+		i++;
+
+	}
+
+	putchar(10);
+
+	return(0);
+}
