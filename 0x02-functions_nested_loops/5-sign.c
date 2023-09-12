346d25a2567416b4b1baf6039f6ad6b5ba825b49
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-12 14:54:10 -0700

    FIX

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints various characters based on the sign of input
  *
- * %n: the number to check
+ * @n: the number to check
  *
  * Return: 1 and print + if n is greater than zero
  * 0 and print 0 if n is zero
@@ -22,7 +22,7 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	else if (n < 0) 
+	else if (n < 0)
 	{
 		_putchar('-');
 	}
