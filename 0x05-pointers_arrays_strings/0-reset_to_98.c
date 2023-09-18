c466dc77e4a31d88b725a92a9ccd4b64b6f3b1e6
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-09-18 12:49:20 -0700

    ADD 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the value of the variable 
+ * the pointer points to to 98
+ * @n: the variable to be reduced to
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
