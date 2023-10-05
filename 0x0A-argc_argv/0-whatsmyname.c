098e044b9e3f9bf021acd8ef1dde29cf4c1b2ff0
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-10-05 06:50:09 -0700

    ADD 0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,18 +4,14 @@
  * main - prints program name and its arguments
  * @argc: argument counter
  * @argv: argument array
+ *
+ * Return: Always zero
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s ", argv[i]);
-	}
-
-	printf("\n");
+	(void) argc;
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
