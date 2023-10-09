e511b0c2b123e0eb3264483c959fb164b935704c
Temitope Akinsunmade <temitopeakinsunmade08@gmail.com>
2023-10-08 22:07:27 -0700

    ADD first task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - function that allocates memory 
+ * @b: size passed in
+ *
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
